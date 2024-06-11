f5716eaf3b8e6f6eb23b8d1ec1933d9e8ee244cd
yashs <yashs087@gmail.com>
2024-06-11 14:37:40 +0530

    SearchInRotatedArray

--- DS/LinkedList.java
+++ DS/LinkedList.java
@@ -36,6 +36,46 @@ public class LinkedList {
         head.next = temp;
     }
 
+    static void delete(Node head, int data){
+        Node current=head;
+        if(head.data==data){
+            head.data=head.next.data;
+            head.next=head.next.next;
+            return;
+        }
+
+        while(current.next != null && current.next.data!=data ){
+            current=current.next;
+        }
+        if(current.next != null && current.next.data ==data){
+            current.next=current.next.next;
+        }
+    }
+
+    static void reverse(Node head){
+        Node prev=null;
+        Node current=head;
+
+        if(current.next == null ){
+            return;
+        }
+        while(current.next != null){
+            int data = current.data;
+            Node temp = new Node(data);
+            temp.next = prev;
+            prev = temp;
+            current=current.next;
+        }
+        int data = current.data;
+        Node temp = new Node(data);
+        temp.next = prev;
+        prev = temp;
+        head.data = prev.data;
+        head.next = prev.next;
+
+    }
+
+
     static void display(Node head){
         while(head.next!=null){
             System.out.print(head.data+" -> ");
@@ -52,6 +92,14 @@ public class LinkedList {
         insert(head, 9);
         insertAtHead(head,13);
         insertAtHead(head,11);
+        display(head);
+        delete(head,4);
+        display(head);
+        delete(head,11);
+        display(head);
+        delete(head,9);
+        display(head);
+        reverse(head);
         display(head);
     }
 }
